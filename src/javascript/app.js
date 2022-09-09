595af7f43dcda344e72339fd68343010d7df1b41
Andy Leverenz <andy@justalever.com>
2022-09-09 14:12:19 -0500

    Revert form junk

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -2,9 +2,6 @@ import { Application } from "@hotwired/stimulus"
 
 import CanvasController from "./controllers/canvas_controller"
 
-import SubscribeController from "./controllers/subscribe_controller"
-
 window.Stimulus = Application.start()
 
 Stimulus.register("canvas", CanvasController)
-Stimulus.register("subscribe", SubscribeController)
